c816236ee4c530a6f4d3b57f1fe670c775bf7848
Humaira Zafar <>
2022-05-14 20:09:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let movies = ['ddlj', 'jab payar kiya to','qayamat se qayamt tak'];
+let firstMovie = movies[0];
+// console.log(firstMovie)
+   movies.push('interstellar', 'miss congenilaity');
+
+movies.unshift('star wars');
+console.log(movies);
